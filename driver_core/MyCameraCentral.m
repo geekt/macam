fa2c9d46855abfd2e3e83974c5284695ba63d0a5
hxr <hxr>
2006-03-07 04:58:19 +0000

    Decided that drivers should really have that part of the class name and filename.

--- driver_core/MyCameraCentral.m
+++ driver_core/MyCameraCentral.m
@@ -15,7 +15,7 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- $Id: MyCameraCentral.m,v 1.32 2006/03/06 17:24:28 hxr Exp $
+ $Id: MyCameraCentral.m,v 1.33 2006/03/07 05:02:48 hxr Exp $
  */
 
 #include <CoreFoundation/CoreFoundation.h>
@@ -49,8 +49,8 @@
 #import "SQ905.h"
 #import "SQ930C.h"
 #import "MyPixartDriver.h"
-#import "PAC7311.h"
-#import "SPCA5XX.h"
+#import "PAC7311Driver.h"
+#import "SPCA5XXDriver.h"
 #include "unistd.h"
     
 
@@ -216,11 +216,11 @@ MyCameraCentral* sharedCameraCentral=NULL;
     [self registerCameraDriver:[SQ905 class]];
     [self registerCameraDriver:[SQ930C class]];
     [self registerCameraDriver:[MyPixartDriver class]];
-    [self registerCameraDriver:[PAC7311 class]];
+    [self registerCameraDriver:[PAC7311Driver class]];
     [self registerCameraDriver:[OV518Driver class]];
     [self registerCameraDriver:[OV518PlusDriver class]];
     [self registerCameraDriver:[OV519Driver class]];
-//    [self registerCameraDriver:[PAC207 class]];
+//    [self registerCameraDriver:[PAC207Driver class]];
     
 //    [self registerCameraDriver:[MyIntelPCCameraPro class]];
 //    [self registerCameraDriver:[MyIntelPCCamera class]];

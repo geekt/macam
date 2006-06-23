ae22b8c95fe2eb8ef54ac96d58211d57b18a6e16
hxr <hxr>
2006-06-23 03:29:03 +0000

    Added a CyYeGMgConverter to the MyQCVCDriver.

--- cameras/MyQCProBeigeDriver.h
+++ cameras/MyQCProBeigeDriver.h
@@ -18,7 +18,7 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- $Id: MyQCProBeigeDriver.h,v 1.3 2005/09/13 20:15:06 hxr Exp $
+ $Id: MyQCProBeigeDriver.h,v 1.4 2006/06/23 03:29:03 hxr Exp $
  */
 
 #import <Cocoa/Cocoa.h>
@@ -99,6 +99,9 @@
 
 
 @interface MyQCVCDriver : MyQCProBeigeDriver 
+{
+    CyYeGMgConverter * CYGM;
+}
 
 + (NSArray*) cameraUsbDescriptions;
 
